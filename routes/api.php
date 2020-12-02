b8b88dd5612976d2ea7b906195d161b3343ddebd
JoelMite <35474476+JoelMite@users.noreply.github.com>
2020-12-01 22:42:43 -0500

    Proyecto V7 Laravel Passport

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::namespace('Api')->group(function () {
+  Route::post('login', 'AuthController@login');
+  Route::post('signup', 'AuthController@signUp');
+
+  Route::middleware('auth:api')->group(function () {
+    Route::get('logout', 'AuthController@logout');
+    Route::get('user', 'AuthController@user');
+
+      });
+
+});
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
