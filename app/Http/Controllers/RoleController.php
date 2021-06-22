f0bd5babb10457be25665f3fae3824ea9cf5b20b
JoelMite <35474476+JoelMite@users.noreply.github.com>
2021-06-21 21:13:51 -0500

    cambio en la vista rol - version 12

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -33,14 +33,14 @@ class RoleController extends Controller
       //return dd($permissions_all);
       $permissions_patient = Permission::where('name', 'LIKE', '%Paciente%')->orWhere('name', 'LIKE', '%Perfil%')->get(); // 4 resultados
       //$permissions_patient = $permissions_all->where('name', 'LIKE', '%paciente%')->orWhere('name', 'LIKE', '%perfil%')->get(); // 4 resultados
-      $permissions_doctor = Permission::where('name', 'LIKE', '%medico%')->where('name', 'NOT LIKE', '%dashboard%')->orWhere('name', 'LIKE', '%perfil%')->orWhere('name', 'LIKE', '%horario%')->get(); // 6 resultados
-      $permissions_role = Permission::where('name', 'LIKE', '%rol%')->get(); // 4 resultados
-      $permissions_specialty = Permission::where('name', 'LIKE', '%especialidad%')->get(); // 4 resultados
-      $permissions_user = Permission::where('name', 'LIKE', '%usuario%')->get(); // 2 resultados
-      $permissions_history_clinic = Permission::where('name', 'LIKE', '%clinica%')->get(); // 4 resultados
-      $permissions_consultation_appointment_medical = Permission::where('name', 'LIKE', '%medica%')->where('name', 'NOT LIKE', '%(Paciente)%')
+      $permissions_doctor = Permission::where('name', 'LIKE', '%Medico%')->where('name', 'NOT LIKE', '%Dashboard%')->orWhere('name', 'LIKE', '%Perfil%')->orWhere('name', 'LIKE', '%Horario%')->get(); // 6 resultados
+      $permissions_role = Permission::where('name', 'LIKE', '%Rol%')->get(); // 4 resultados
+      $permissions_specialty = Permission::where('name', 'LIKE', '%Especialidad%')->get(); // 4 resultados
+      $permissions_user = Permission::where('name', 'LIKE', '%Usuario%')->get(); // 2 resultados
+      $permissions_history_clinic = Permission::where('name', 'LIKE', '%Clinica%')->get(); // 4 resultados
+      $permissions_consultation_appointment_medical = Permission::where('name', 'LIKE', '%Medica%')->where('name', 'NOT LIKE', '%(Paciente)%')
       ->where('name', 'NOT LIKE', '%Pacientes%')->get(); // 9 resultados
-      $permissions_dashboard = Permission::where('name', 'LIKE', '%dashboard%')->get(); // 2 resultados
+      $permissions_dashboard = Permission::where('name', 'LIKE', '%Dashboard%')->get(); // 2 resultados
 
       //return $permissions_patient;
       return view('roles.create', compact('permissions_patient', 'permissions_doctor', 'permissions_role', 'permissions_specialty', 'permissions_user', 'permissions_history_clinic', 'permissions_consultation_appointment_medical', 'permissions_dashboard'));
